929116fac99940825760eb9e40e39d986edb5c8d
Furtsy <43937364+Furtsy@users.noreply.github.com>
2020-07-30 02:13:34 +0300

    Create chart.js

---
+++ js/chart.js
@@ -0,0 +1,6 @@
+var Chart = require('chart.js');
+var myChart = new Chart(ctx);
+var ctx = document.getElementById('myChart');
+var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = $('#myChart');
+var ctx = 'myChart';
